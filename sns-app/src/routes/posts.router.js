656b14077b96f7eebafb3c93b4558b52cff139ce
giung-kim <80654070+giung-kim@users.noreply.github.com>
2024-04-17 20:46:33 +0900

    add sns-app

---
+++ sns-app/src/routes/posts.router.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const { checkAuthenticated } = require('../middleware/auth');
+const router=express.Router();
+const Post=require('../models/posts.model');
+const Comment=require('../models/comments.model');
+
+router.get('/', checkAuthenticated, (req, res) => {
+    Post.find()
+        .populate('comments')
+        .sort({ createdAt: -1 })
+        .exec((err, posts) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('posts', {
+                    posts: posts,
+                    currentUser: req.user
+                });
+            }
+        })
+})
+
+module.exports = router;
