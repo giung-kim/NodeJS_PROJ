ef53e2bb1f36fd3c62a1259a46e56d87f7a91ad7
giung-kim <80654070+giung-kim@users.noreply.github.com>
2024-04-21 22:47:07 +0900

    like/friends

--- sns-app/src/routes/friends.router.js
+++ sns-app/src/routes/friends.router.js
@@ -1,4 +1,20 @@
 const express=require('express');
+const { checkAuthenticated } = require('../middleware/auth');
+const User=require('../models/users.model');
 const router=express.Router();
+
+
+router.get('/', checkAuthenticated, (req, res) => {
+    User.find({}, (err, users) => {
+        if (err) {
+            req.flash('error', '유저를 가져오는데 에러가 발생했습니다.');
+            res.redirect('/posts');
+        } else {
+            res.render('friends', {
+                users: users
+            })
+        }
+    })
+})
 
 module.exports =router;
--- sns-app/src/server.js
+++ sns-app/src/server.js
@@ -102,7 +102,7 @@ app.use('/posts',postsRouter);
 app.use('/posts/:id/comments', commentsRouter);
 app.use('/profile/:id', profileRouter);
 app.use('/friends', friendsRouter);
-app.use('/posts/:id/like',likeRouter);
+app.use(likeRouter);
 //#endregion
 
 
